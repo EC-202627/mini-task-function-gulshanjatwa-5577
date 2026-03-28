ba525bb822e5b773829ca922abdf2f1ca1c49aa5
Gulshan Jatwa <gulshan.jatwa@avantika.edu.in>
2026-03-28 16:35:41 +0000

    edite

--- Problem_02.py
+++ Problem_02.py
@@ -5,8 +5,10 @@ def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
     print(f"Fine: Rs. {fine_amount}")
     return fine_amount
 
-if __name__ == "__main__":
+def start():
     title = input().strip()
     days = int(input().strip())
     rate = float(input().strip())
     calculate_fine(title, days, daily_rate=rate)
+
+start()
